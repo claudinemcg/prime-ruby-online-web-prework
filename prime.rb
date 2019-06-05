a029623a508d052497243ee357b43e14ef083d00
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-05 09:22:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,10 @@ def prime?(int)
   i = 0
   arr = [2..int] 
   (2...int).each do
-    if int % arr[i] == 0
-      return false
+    if int <= 2
+      return flase
+    elsif int %  == 0
+      
     else return true
     end
   end
